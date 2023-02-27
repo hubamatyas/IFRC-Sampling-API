138539d9f04bc21f7445c94be7067323586fa824
Matyas Huba <hubamatyas@gmail.com>
2023-02-27 18:17:44 +0000

    Refactored database

--- sampling/models.py
+++ sampling/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 class State(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=1000, null=True)
-    parent_state = models.ForeignKey('self', on_delete=models.CASCADE)
-    term = models.CharField(max_length=500)
+    name = models.CharField(max_length=1000)
+    parent_state = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    term = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Option(models.Model):
-    state = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    child_state = models.ForeignKey(State, on_delete=models.CASCADE, related_name='child_state')
+    child_state = models.ForeignKey(State, on_delete=models.CASCADE, null=True, related_name='child_state', blank=True)
 
     def __str__(self):
         return self.name
